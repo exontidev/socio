805bf9cb01835d5d6f4492fa56c6c1293ea294a9
exontidev <me@exonti.dev>
2026-06-22 18:40:42 +0200

    config + notifier + status codes

---
+++ services/chatroom/src/api/ws/notifier.rs
@@ -0,0 +1,42 @@
+use crate::{
+    api::ws::{
+        RelaySender,
+        staus_codes::{NotifyCode, WebSocketError},
+    },
+    requests::{RequestId, WebSocketMessage},
+};
+
+pub struct Notifier {
+    pub id: Option<RequestId>,
+    pub tx: RelaySender,
+}
+
+// FIX: duplicate
+type Result = core::result::Result<NotifyCode, WebSocketError>;
+
+impl Notifier {
+    pub fn result(&self, result: Result) {
+        let _ = match result {
+            Ok(success) => self.code(success),
+            Err(err) => self.error(err),
+        };
+    }
+
+    pub fn code(&self, code: NotifyCode) {
+        self.tx.send(WebSocketMessage::Notify {
+            request_id: self.id,
+            code: code,
+        });
+    }
+
+    pub fn error(&self, err: WebSocketError) {
+        self.tx.send(WebSocketMessage::Error {
+            request_id: self.id,
+            code: err,
+        });
+    }
+
+    pub fn set_id(&mut self, id: RequestId) {
+        self.id = Some(id);
+    }
+}
